3e5d119a6492c07019b3b9229b7ebc3b2f1c6e05
Josh <haojhesyu@gmail.com>
2014-08-05 22:21:07 +0800

    Shorten non-recursion versoin code

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -11,7 +11,7 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r2, r3, r4, r5, lr}
 
 	@ R4 = R0 - 0 (update flags)
 	sub r4, r0, #0
@@ -19,13 +19,7 @@ fibonacci:
 	@ Compare R0 with 0
 	@ if(R0 <= 0) goto .L3 (which returns 0)
 	cmp r0, #0
-	beq .L3
-	blt .L3
-	
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-	cmp r4, #1
-	beq .L4
+	ble .L3
 	
 	@ Do Loop add
 	mov r2, #0
@@ -39,15 +33,11 @@ fibonacci:
 	cmp r2, r4
 	blt .LADD
 	
-	pop {r3, r4, r5, pc}		@EPILOG
+	pop {r2, r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
 .L3:
 	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
 	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
